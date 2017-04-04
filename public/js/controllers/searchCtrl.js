1542a9ae0d25519d2e4e00f414fc01387547c2e4
Kiwicannon <stevencannon01@gmail.com>
2017-04-04 17:07:41 -0600

    api search endpoints

--- public/js/controllers/searchCtrl.js
+++ public/js/controllers/searchCtrl.js
@@ -1,24 +1,15 @@
 angular.module('goodSees')
-    .controller('searchCtrl', function($scope, tmdbService){
-// console.log('this is working')
-    //      $scope.test = 'controller is working'
-    // $scope.test1 = tmdbService.test1
-    //  tmdbService.getMovies().then(function(response){
-         
-    //      $scope.movies = response.results
-    //      console.log($scope.movies)
-    //  })
+    .controller('searchCtrl', function ($scope, tmdbService) {
+   /*--------------------------------------------------------------------*
+                     Event Handlers 
+    *--------------------------------------------------------------------*/ 
+        $scope.searchMovieByTitle = function (keyEvent) {
+            if (keyEvent.which === 13) {
+                tmdbService.searchMovieByTitle($scope.searchStuff)
+                .then(function (movieInfo) {
+                    $scope.search = movieInfo.data
+                })
+            }
+        }
 
-     $scope.searchStuff
-     $scope.myfunc = function(keyEvent) {
-         if (keyEvent.which === 13){
-         
-         
-     tmdbService.getMoviesBy($scope.searchStuff).then(function(movieInfo){
-            $scope.search = movieInfo.results
-            //   console.log($scope.search)
-                   })
-         }
-     }
-    
     });
--- public/js/services/tmdbService.js
+++ public/js/services/tmdbService.js
@@ -1,40 +1,8 @@
 angular.module('goodSees')
     .service('tmdbService', function($http){
     var baseUrl = 'https://api.themoviedb.org/3/'
-    var key = '?api_key=dacb04943975d3dddd961e4b876f8019'
     var discoverPageNum = 1
-    var thing
-    
- //get movies by popularity or for a discover page
 
+    this.searchMovieByTitle = title => $http.get(`/searchMovieByTitle/${title}`)
 
-    // this.getMovies = function(){
-    //     return $http({
-    //         method: "GET",
-    //         url: baseUrl + 'discover/movie' + key + '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + discoverPageNum
-
-    //     }).then(function(response){
-            
-    //         if(response.status===200){
-    //             // console.log(response.data)
-    //             return response.data
-    //         }return "ERROR!!!"
-    //     })
-    //     console.log(url)
-    //     console.log(baseUrl + 'discover/movie' + key + '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + discoverPageNum
-    //         )
-    // }
-
-    this.getMoviesBy =function(thing){
-        return $http({
-            method: "GET",
-            url: baseUrl + 'search/movie' + key +  '&language=en-US&query='+thing+'&page=1' 
-        
-    }).then(function(response){
-            if(response.status===200){
-                // console.log(response.data)
-                return response.data
-            }return "ERROR!!!" + response.status
-        })
-    }
 });
