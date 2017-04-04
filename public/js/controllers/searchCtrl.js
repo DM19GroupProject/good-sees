4933dbf66eb0918b4116648f6cc1dc26e48f31e3
Kiwicannon <stevencannon01@gmail.com>
2017-04-04 14:37:09 -0600

    api search

--- public/js/controllers/searchCtrl.js
+++ public/js/controllers/searchCtrl.js
@@ -1,5 +1,24 @@
 angular.module('goodSees')
-    .controller('searchCtrl', function($scope, mainService){
+    .controller('searchCtrl', function($scope, tmdbService){
+// console.log('this is working')
+    //      $scope.test = 'controller is working'
+    // $scope.test1 = tmdbService.test1
+    //  tmdbService.getMovies().then(function(response){
+         
+    //      $scope.movies = response.results
+    //      console.log($scope.movies)
+    //  })
 
-        $scope.test = mainService.test;
+     $scope.searchStuff
+     $scope.myfunc = function(keyEvent) {
+         if (keyEvent.which === 13){
+         
+         
+     tmdbService.getMoviesBy($scope.searchStuff).then(function(movieInfo){
+            $scope.search = movieInfo.results
+            //   console.log($scope.search)
+                   })
+         }
+     }
+    
     });
