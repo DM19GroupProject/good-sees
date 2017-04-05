c37aa9f52602f64839a6966f8f04ce7f92fe0f05
1nsomniac <samthakala@gmail.com>
2017-04-05 12:04:45 -0600

    put a schema together, working on endpoints now. also made some dummy data

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,77 @@
-DROP TABLE IF EXISTS user, friends, top_friends, fav_movies, seen_movies, to_see_movies;
+DROP TABLE IF EXISTS users, friends, fav_movies, seen_movies, to_see_movies, movie_ratings;
 
-CREATE TABLE user(
-    user_id varchar(250) PRIMARY KEY,
-    first_name varchar(25),
-    last_name varchar(25)
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    fb_id BIGINT,
+    first_name varchar(50),
+    has_comments BOOLEAN,
+    first_comment_snippet TEXT,
+    last_name varchar(50),
+    picture_url TEXT,
+    fb_friends TEXT,
+    google_id TEXT,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO users
+    (fb_id, first_name, last_name, picture_url, fb_friends, google_id)
+VALUES(1197287247035846, 'Joe', 'Dirt', 'http:
+//bit.ly/2nbmoL6', '1197987247035846, 11972872447035846,1197287247035826, 1797287247035846', null);
+
+CREATE TABLE friends
+(
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT,
+    friend_id BIGINT,
+    rank INT,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE to_see_movies
+(
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT,
+    movie_id BIGINT,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE seen_movies
+(
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT,
+    movie_id BIGINT,
+    has_comment BOOLEAN,
+
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE fav_movies
+(
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT,
+    movie_id BIGINT,
+    has_comment BOOLEAN,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE movie_ratings
+(
+    id SERIAL PRIMARY KEY,
+    movie_id BIGINT,
+    thumbs_up INT,
+    thumbs_sideways INT,
+    thumbs_down INT,
+    user_id BIGINT,
+    comment_title TEXT,
+    comment TEXT,
+    is_fav BOOLEAN,
+    is_seen BOOLEAN,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO movie_ratings
+    (movie_id, thumbs_up, thumbs_sideways, thumbs_down, user_id, comment_title, comment,is_fav,is_seen)
+VALUES
+    (4523, 0, 0, 1, 1197287247035846, 'wonderful movie', 'just kidding. not worth the plastic to make this dvd', false, true),
+    (4523, 0, 1, 0, 1197287247035849, 'ehh', 'the songs are ok. everything else is not', false, true);
