6e7c9765eb8380029a681c677fd28304d459656f
1nsomniac <samthakala@gmail.com>
2017-04-06 17:00:02 -0600

    added endpoints for sql

--- db/feed/get_new_feed.sql
+++ db/feed/get_new_feed.sql
@@ -3,3 +3,32 @@
 -- returns list of movie ids paired with recommendation data, seen/to see/favs list data, and thumbs data
 
 -- stretch feature, implement sessions/reload state type of page organizations. like pull up and reload feed, vs just paging out the stuff that loaded on the first page-visit time.
+
+
+SELECT DISTINCT users.first_name,
+  friends.user_id,
+  friends.friend_id,
+  movie_rating.user_id,
+  movie_rating,
+  movie_rating.date_created
+FROM users
+  JOIN friends
+  ON friends.friend_id = users.fb_id
+  JOIN movie_rating
+  ON movie_rating.user_id = friends.friend_id
+WHERE friends.user_id = $1
+ORDER BY movie_rating.date_created;
+
+
+
+-- select 
+
+-- user id from friend
+
+-- right join on friend id an rating from movire ratings
+
+-- movie ratings by friend id = better idea
+
+-- easiest below
+-- group by user id
+-- group by movie ratings
--- db/movie/get_reviews.sql
+++ db/movie/get_reviews.sql
@@ -1,2 +1,3 @@
 -- takes in movie id
 -- returns null or set of review content/review title/review date and time/reviewer name/reviewer picture/seen/fav/want to see/thumbs and recommendation too
+
