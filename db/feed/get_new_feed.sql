b33ad98efcc94a160b7c2c50f06880180cbf6b08
1nsomniac <samthakala@gmail.com>
2017-04-07 16:19:18 -0600

    everything but post user sql endpoint is done!

--- db/feed/get_new_feed.sql
+++ db/feed/get_new_feed.sql
@@ -1,20 +1,26 @@
--- takes in user id, and page needed(zero for first one)
+-- takes in user id
+-- $1 user id
+-- returns friends activity time sorted with first and last name, picture url, id, movie id, comment title, date of last activity with that movie, if thumb up, if thumb sideways, if thumb down, if recommended, if fav, if seen, and if to see.
 
--- returns list of movie ids paired with recommendation data, seen/to see/favs list data, and thumbs data
-
--- stretch feature, implement sessions/reload state type of page organizations. like pull up and reload feed, vs just paging out the stuff that loaded on the first page-visit time.
-
-
-SELECT DISTINCT users.first_name,
-  friends.user_id,
+SELECT DISTINCT
+  users.first_name,
+  users.last_name,
+  users.picture_url,
   friends.friend_id,
-  movie_rating.user_id,
-  movie_rating,
-  movie_rating.date_created
+  movie_rating.movie_id,
+  movie_rating.comment_title,
+  movie_rating.date_created,
+  movie_rating.thumb_up,
+  movie_rating.thumb_sideways,
+  movie_rating.thumb_down,
+  movie_rating.recommends,
+  movie_rating.fav,
+  movie_rating.seen,
+  movie_rating.to_see
 FROM users
   JOIN friends
   ON friends.friend_id = users.fb_id
   JOIN movie_rating
   ON movie_rating.user_id = friends.friend_id
-WHERE friends.user_id = $1
+WHERE friends.user_id = 1197287247035846
 ORDER BY movie_rating.date_created;
