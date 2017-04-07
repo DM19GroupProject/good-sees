a55a0d4a57d3008f7b198dfda9bd980f6207262b
1nsomniac <samthakala@gmail.com>
2017-04-07 17:58:43 -0600

    finished all necessary queries. all have descriptions.

--- db/login/post_new_user_info.sql
+++ db/login/post_new_user_info.sql
@@ -1,2 +1,9 @@
--- posts all known data from first fb login
--- takes in all fb data
+-- takes in fb id, first name, last name, and picture_url.
+-- $1 fb id
+-- $2 first name
+-- $3 last name
+-- $4 picure_url
+-- will throw error if user exists.
+
+INSERT INTO users (fb_id, first_name, last_name, picture_url)
+VALUES ($1, $2, $3, $4);
