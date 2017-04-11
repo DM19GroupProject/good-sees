474c2a96df2649185cbe10dad6c7c7a7939b8dfc
Maddie Humphreys <heymaddieh@users.noreply.github.com>
2017-04-11 16:44:15 -0600

    yay styling on link tabs is working yayayayayay

--- public/js/controllers/profileCtrl.js
+++ public/js/controllers/profileCtrl.js
@@ -1,5 +1,7 @@
 angular.module('goodSees')
     .controller('profileCtrl', function($scope, mainService){
-
+        $scope.link = 'default'
         $scope.test = mainService.test;
+
+        
 });
