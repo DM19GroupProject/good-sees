bbdfa0a96ce4befb946b11d745cc3ca3a29bc313
Kiwicannon <stevencannon01@gmail.com>
2017-04-18 16:49:56 -0600

    friend page styled

--- public/js/services/mainService.js
+++ public/js/services/mainService.js
@@ -166,3 +166,5 @@ angular.module('goodSees')
                 })
         }
     });
+
+    
--- public/js/services/userService.js
+++ public/js/services/userService.js
@@ -8,15 +8,25 @@ angular.module('goodSees')
     
     function getUserId() {
       return $http.get(`/auth/me`).then(res => {
-        console.log('I ran',res.status, res.data, res)
+        
         // if(res.status !== 200) {$state.go('login')}
         userId = res.data
         return res.data
       })
     }
     
-  });
 
+   /*--------------------------------------------------------------------*
+                             Friends page Endpoints
+        *--------------------------------------------------------------------*/
+
+
+        this.getFriends = id => {
+            return $http.get(`/getFriends/${id}`)
+            
+        }
+
+  });
 // make sure that you inject userService, and then wrap all call relating to the user id in this function >>
 // userService.getUserId().then(function (userId) { 
 //   $http.get('/whatever/' + userId + '/' + 'whatever').then(res => {
